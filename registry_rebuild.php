a7cbc542c5ebb630fab1c410023010f73f3b2bf2
Randy Fay <randy@randyfay.com>
2011-06-21 08:07:17 -0600

    Issue #1195144 by rfay: Make sure current directory is set correctly and do full bootstrap

--- registry_rebuild.php
+++ registry_rebuild.php
@@ -1,9 +1,12 @@
 <?php
 
 /**
- * Root directory of Drupal installation.
+ * Root directory of Drupal installation. Note that you can change this
+ * if you need to. It just has to point to the actual root. This assumes
+ * that the php file is being run in sites/all/modules/registry_rebuild.
  */
 define('DRUPAL_ROOT', realpath(getcwd() . '/../../../..'));
+chdir(DRUPAL_ROOT);
 print "DRUPAL_ROOT is " . DRUPAL_ROOT . ".<br/>\n";
 
 global $_SERVER;
@@ -11,12 +14,17 @@ $_SERVER['REMOTE_ADDR'] = 'nothing';
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 require_once DRUPAL_ROOT . '/includes/common.inc';
 require_once DRUPAL_ROOT . '/includes/registry.inc';
+require_once DRUPAL_ROOT . '/includes/entity.inc';
+require_once DRUPAL_ROOT . '/modules/system/system.module';
 
 require_once DRUPAL_ROOT . '/includes/database/query.inc';
 require_once DRUPAL_ROOT . '/includes/database/select.inc';
 
+print "Bootstrapping to DRUPAL_BOOTSTRAP_SESSION<br/>\n";
 drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
 
+// This section is not functionally important. It's just getting the
+// registry_parsed_files() so that it can report the change.
 $connection_info = Database::getConnectionInfo();
 $driver = $connection_info['default']['driver'];
 require_once DRUPAL_ROOT . '/includes/database/' . $driver . '/query.inc';
@@ -27,8 +35,17 @@ cache_clear_all('lookup_cache', 'cache_bootstrap');
 cache_clear_all('variables', 'cache_bootstrap');
 cache_clear_all('module_implements', 'cache_bootstrap');
 
+print "Doing registry_rebuild() in DRUPAL_BOOTSTRAP_SESSION<br/>\n";
+registry_rebuild();   // At lower level
+
+print "Bootstrapping to DRUPAL_BOOTSTRAP_FULL<br/>\n";
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+print "Doing registry_rebuild() in DRUPAL_BOOTSTRAP_FULL<br/>\n";
 registry_rebuild();
 $parsed_after = registry_get_parsed_files();
 
+print "Flushing all caches<br/>\n";
+drupal_flush_all_caches();
+
 print "There were " . count($parsed_before) . " files in the registry before and " . count($parsed_after) . " files now.<br/>\n";
-print "If you don't see any crazy fatal errors, your registry has been rebuilt. You will probably want to flush your caches now.<br/>\n";
+print "If you don't see any crazy fatal errors, your registry has been rebuilt.<br/>\n";
