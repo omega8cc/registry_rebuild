4e14fc50f41673446e0d00c1b78e9bac6bfce155
joachim <joachim@107701.no-reply.drupal.org>
2011-11-18 10:28:10 -0700

    Issue #1342352 by joachim: Add a more ambitious search for DRUPAL_ROOT

--- registry_rebuild.php
+++ registry_rebuild.php
@@ -5,7 +5,7 @@
  * if you need to. It just has to point to the actual root. This assumes
  * that the php file is being run in sites/all/modules/registry_rebuild.
  */
-define('DRUPAL_ROOT', realpath(getcwd() . '/../../../..'));
+define('DRUPAL_ROOT', define_drupal_root());
 chdir(DRUPAL_ROOT);
 print "DRUPAL_ROOT is " . DRUPAL_ROOT . ".<br/>\n";
 define('MAINTENANCE_MODE', 'update');
@@ -24,6 +24,25 @@ print "Bootstrapping to DRUPAL_BOOTSTRAP_SESSION<br/>\n";
 drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
 
 registry_rebuild_rebuild();
+
+/**
+ * Find the drupal root directory by looking in parent directories.
+ * If unable to discover it, fail and exit.
+ */
+function define_drupal_root() {
+  // This is the smallest number of directories to go up: from /sites/all/modules/registry_rebuild.
+  $parent_count = 4;
+  // 8 seems reasonably far to go looking up.
+  while ($parent_count < 8) {
+    $dir = realpath(getcwd() . str_repeat('/..', $parent_count));
+    if (file_exists($dir . '/index.php')) {
+      return $dir;
+    }
+    $parent_count++;
+  }
+  print "Failure: Unable to discover DRUPAL_ROOT. You may want to explicitly define it near the top of registry_rebuild.php";
+  exit(1);
+}
 
 /**
  * Before calling this we need to be bootstrapped to DRUPAL_BOOTSTRAP_DATABASE.
